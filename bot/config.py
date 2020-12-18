5b67411b30b7c6dcc9f84b4563142a86cb316156
Dariushg75 <44521775+Dariushg75@users.noreply.github.com>
2020-12-18 11:53:17 +0330

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,7 +3,7 @@ import json
 from distutils.util import strtobool as stb
 
 # --------------------------------------
-BOT_TOKEN = "1363610141:AAHPqiH17b_OpqcIO0hDeFWQ6Kx-4vuCWeA"
+BOT_TOKEN = "1196860469:AAGmQeZA8ojdx_ix343HyT4PO4kN4zAvDYk"
 GDRIVE_FOLDER_ID = "0AJl33hRBxwVZUk9PVA"
 # Default folder id.
 OWNER_ID = 430750847
